fb00d410e0a16545c4b00032466152466741bd20
tiagosathler <sathler@gmail.com>
2022-03-20 15:34:34 -0300

    Adiciona um parágrafo

--- src/App.js
+++ src/App.js
@@ -17,24 +17,27 @@ function App() {
   if (loading) return <h1>loading</h1>;
 
   return (
-    <table>
-      <tr>
-        {Object.keys(data[0]).map(
-          (key) => (<th key={ Math.random() }>{key}</th>),
-        )}
-      </tr>
-      <tbody>
-        {data.map((rowInfo) => (
-          <tr key={ Math.random() }>
-            {
-              Object.values(rowInfo).map(
-                (cellInfo) => (<td key={ Math.random() }>{cellInfo}</td>),
-              )
-            }
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div>
+      <table>
+        <tr>
+          {Object.keys(data[0]).map(
+            (key) => (<th key={ Math.random() }>{key}</th>),
+          )}
+        </tr>
+        <tbody>
+          {data.map((rowInfo) => (
+            <tr key={ Math.random() }>
+              {
+                Object.values(rowInfo).map(
+                  (cellInfo) => (<td key={ Math.random() }>{cellInfo}</td>),
+                )
+              }
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    <p>Teste</p>
+    </div>
   );
 }
 
